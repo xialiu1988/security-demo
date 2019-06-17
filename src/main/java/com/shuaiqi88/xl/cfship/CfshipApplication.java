c00910b87bc38290589e4851f4a4003502b28aa3
xialiu1988 <liuxia198896@gmail.com>
2019-06-17 15:06:21 -0700

    firt

---
+++ src/main/java/com/shuaiqi88/xl/cfship/CfshipApplication.java
@@ -0,0 +1,13 @@
+package com.shuaiqi88.xl.cfship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfshipApplication.class, args);
+	}
+
+}
